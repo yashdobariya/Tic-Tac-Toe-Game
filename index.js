a967d71fb476d970e8750d0638c0e2c4ac9ea098
yashdobariya888@gmail.com <yashdoabriya888@gmail.com>
2021-08-03 15:53:44 +0530

    reset function implimention

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let pl_One_Data = document.getElementById("pl_one");
 let pl_Two_Data = document.getElementById("pl_two");
 let reset_Data = document.getElementById("reset");
-let game_Table = document.getElementById("game_table")
-let cell_Data = document.getElementsByClassName("game_cell")
-let win_Data = document.getElementById("win")
-// let cell_Data_Record = [[], [], []];
-let cell_Data_Record = [["","",""], ["","",""], ["","",""]];
+let game_Table = document.getElementById("game_table");
+let cell_Data = document.getElementsByClassName("game_cell");
+let win_Data = document.getElementById("win");
+let cell_Data_Record = [[], [], []];
 let playersign = '';
 let playerWin;
+let signCount = 0;
 
 const selectPlayer = (event) => {
     
@@ -15,7 +15,7 @@ const selectPlayer = (event) => {
         pl_Two_Data.style.visibility = "hidden";
         reset_Data.style.visibility = "visible";
         reset_Data.style.marginLeft = "-80px";
-        playersign = "X"
+        playersign = "X";
         
     } else {
         pl_One_Data.style.display = "none";
@@ -27,7 +27,9 @@ const selectPlayer = (event) => {
     
 }
 
-const getsign = (event,ind1,ind2) => {
+const getsign = (event, ind1, ind2) => {
+    signCount += 1;
+    console.log(signCount,"c");
     let sign = event.target;
     let sign_Pointer = document.getElementById(event.target.id);
     sign.innerHTML = playersign;
@@ -38,68 +40,97 @@ const getsign = (event,ind1,ind2) => {
     }   
     sign_Pointer.style.pointerEvents = "none";
     
-    CheckPlayerWin(event,ind1,ind2)
+    CheckPlayerWin(event, ind1, ind2)
+
 }
 
 
 const CheckPlayerWin = (event,ind1,ind2) => {
-
-    playerWin = true;
-
+    // playerWin = true;
     let selected_Cell = event.target;
     console.log(selected_Cell);
     cell_Data_Record[ind1][ind2] = selected_Cell.innerHTML;
+    console.log(cell_Data_Record);
+    if (cell_Data_Record[0][0] === cell_Data[1].innerHTML   && cell_Data_Record[0][1] === cell_Data[2].innerHTML) {
+        win_Data.innerHTML = "You Win";
+        game_Table.style.pointerEvents = "none";
+        // playerWin = true;
+    }
     
-    if ( !cell_Data_Record[0].includes("") && cell_Data_Record[0][0] === cell_Data[1].innerHTML && cell_Data_Record[0][0] === cell_Data[2].innerHTML) {
+    else if (cell_Data_Record[1][0] === cell_Data[4].innerHTML && cell_Data_Record[1][1] === cell_Data[5].innerHTML) {
         win_Data.innerHTML = "You Win";
-        playerWin = true;
+        game_Table.style.pointerEvents = "none";
+        // playerWin = true;
+    }
+
+    else if (cell_Data_Record[2][0] ==  cell_Data[7].innerHTML && cell_Data_Record[2][0] ==  cell_Data[8].innerHTML) {
+        win_Data.innerHTML = "You Win";
+        game_Table.style.pointerEvents = "none";
+        // playerWin = true;
+    }
+
+    else if (cell_Data_Record[0][0] == cell_Data[3].innerHTML && cell_Data_Record[1][0] == cell_Data[6].innerHTML) {
+        win_Data.innerHTML = "You Win";
+        game_Table.style.pointerEvents = "none";
+        // playerWin = true;
+    }
+
+    else if (cell_Data_Record[0][1] == cell_Data[4].innerHTML && cell_Data_Record[1][1] == cell_Data[7].innerHTML) {
+        win_Data.innerHTML = "You Win";
+        game_Table.style.pointerEvents = "none";
+        // playerWin = true;
     }
     
-    else if (cell_Data_Record[1][0] == cell_Data[4].innerHTML && cell_Data_Record[1][0] == cell_Data[5].innerHTML) {
+    else if (cell_Data_Record[0][2] == cell_Data[5].innerHTML && cell_Data_Record[1][2] == cell_Data[8].innerHTML) {    
         win_Data.innerHTML = "You Win";
-        playerWin = true;
-    }
-    else if (cell_Data_Record[2][0] == cell_Data[7].innerHTML && cell_Data_Record[2][0] == cell_Data[8].innerHTML) {
-        win_Data.innerHTML = "You Win";
-        playerWin = true;
-    }
-    else if (cell_Data_Record[0][0] == cell_Data[3].innerHTML && cell_Data_Record[0][0] == cell_Data[6].innerHTML) {
-        win_Data.innerHTML = "You Win";
-        playerWin = true;
-    }
-    else if (cell_Data_Record[0][1] == cell_Data[4].innerHTML && cell_Data_Record[0][1] == cell_Data[7].innerHTML) {
-        win_Data.innerHTML = "You Win";
-        playerWin = true;
-    }
-    else if (cell_Data_Record[0][2] == cell_Data[5].innerHTML && cell_Data_Record[0][2] == cell_Data[8].innerHTML) {    
-        win_Data.innerHTML = "You Win";
-        playerWin = true;
-    }
-    else if (cell_Data_Record[0][0] == cell_Data[4].innerHTML && cell_Data_Record[0][0] == cell_Data[8].innerHTML) {    
-        win_Data.innerHTML = "You Win";
-        playerWin = true;
-    }
-    else if (cell_Data_Record[0][2] == cell_Data[4].innerHTML && cell_Data_Record[0][2] == cell_Data[6].innerHTML) {    
-        win_Data.innerHTML = "You Win";
-        playerWin = true;
+        game_Table.style.pointerEvents = "none";
+        // playerWin = true;    
     }
 
-    else {
-        
-        playerWin = false;
+    else if (cell_Data_Record[0][0] == cell_Data[4].innerHTML && cell_Data_Record[1][1] == cell_Data[8].innerHTML) {    
+        win_Data.innerHTML = "You Win";
+        game_Table.style.pointerEvents = "none";
+        // playerWin = true;
     }
 
-    return playerWin;
+    else if (cell_Data_Record[0][2] == cell_Data[4].innerHTML && cell_Data_Record[1][1] == cell_Data[6].innerHTML) {    
+        win_Data.innerHTML = "You Win";
+        game_Table.style.pointerEvents = "none";
+        // playerWin = true;
+    }
+    else {  
+        if (signCount == 9) {
+            win_Data.innerHTML = "Match Tie";
+        }
+    }
 }
 
 
-const exists = (arr, search) => {
-    return arr.some(row => row.includes(search));
+const reset = () => {
+    pl_One_Data.style.display = "unset";
+    pl_Two_Data.style.visibility = "visible";
+    reset_Data.style.visibility = "hidden";
+    game_Table.style.visibility = "hidden";
+    console.log(game_Table.style.pointerEvents = "auto")
+    win_Data.innerHTML = "";
+    signCount = 0;
+    playersign = '';
+    for (let i = 0; i < cell_Data.length; i++) { 
+        cell_Data[i].innerHTML = "";
+        console.log(cell_Data[i],"f");
+        cell_Data[i].style.pointerEvents = "auto";
+    }
+    cell_Data_Record = [[], [], []];
+    console.log(cell_Data_Record,"reset");
 }
 
-if (!playerWin && !exists(cell_Data_Record,"")) {
-    win_Data.innerHTML = "Match Tie";
-}
+// const exists = (arr, search) => {
+//     return arr.some(row => row.includes(search));
+// }
+
+// if (!playerWin && !exists(cell_Data_Record,"")) {
+//     win_Data.innerHTML = "Match Tie";
+// }
 
 // for (i = 0; i < cell_Data_Record.length; i++) {
     
@@ -114,18 +145,3 @@ if (!playerWin && !exists(cell_Data_Record,"")) {
 //     }
 // }   
 
-
-// const reset = () => {
-//     pl_One_Data.style.display = "unset";
-//     pl_Two_Data.style.visibility = "visible";
-//     reset_Data.style.visibility = "hidden";
-//     game_Table.style.visibility = "hidden";
-//      win_Data.style.display = "none"
-//     playersign = '';
-//     console.log( playersign,"sign");    
-
-//     for (let i = 0; i < cell_Data.length; i++) { 
-//         cell_Data[i].innerHTML = "";
-//     }
-//     cell_Data_Record = [[], [], []];
-// }
