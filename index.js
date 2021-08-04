de830973e22561079d10bd8ac26d0043e232907b
yashdobariya888@gmail.com <yashdoabriya888@gmail.com>
2021-08-04 11:14:59 +0530

    update win player sign and fix bug of player slect button

--- index.js
+++ index.js
@@ -21,32 +21,31 @@ const selectPlayer = (event) => {
     }
     reset_Data.style.visibility = "visible";
     game_Table.style.visibility = "visible";
+    pl_One_Data.style.pointerEvents = "none";
+    pl_Two_Data.style.pointerEvents = "none";
     
 }
 
 const getsign = (event, ind1, ind2) => {
     signCount += 1;
-    // console.log(signCount,"c");
     let sign = event.target;
     let sign_Pointer = document.getElementById(event.target.id);
     sign.innerHTML = playersign;
+    CheckPlayerWin(event, ind1, ind2)
     if (playersign == "X") {
         playersign = "O";
     } else {
         playersign = "X";
     }   
     sign_Pointer.style.pointerEvents = "none";
-    CheckPlayerWin(event, ind1, ind2)
 }
 
 const CheckPlayerWin = (event,ind1,ind2) => {
     let selected_Cell = event.target;
-    // console.log(selected_Cell);
     cell_Data_Record[ind1][ind2] = selected_Cell.innerHTML;
-    // console.log(cell_Data_Record);
+    console.log(cell_Data_Record);
     if (
         (cell_Data_Record[0][0] === cell_Data[1].innerHTML && cell_Data_Record[0][1] === cell_Data[2].innerHTML) ||
-        (cell_Data_Record[1][0] === cell_Data[4].innerHTML && cell_Data_Record[1][1] === cell_Data[5].innerHTML) ||
         (cell_Data_Record[1][0] === cell_Data[4].innerHTML && cell_Data_Record[1][1] === cell_Data[5].innerHTML) ||
         (cell_Data_Record[2][0] === cell_Data[7].innerHTML && cell_Data_Record[2][0] === cell_Data[8].innerHTML) ||
         (cell_Data_Record[0][0] === cell_Data[3].innerHTML && cell_Data_Record[1][0] === cell_Data[6].innerHTML) ||
@@ -57,6 +56,7 @@ const CheckPlayerWin = (event,ind1,ind2) => {
     {   
         win_Data.innerHTML = `Player ${playersign} Win`;
         game_Table.style.pointerEvents = "none";
+        console.log("winner winner chiken dinner");
     }
     else {  
         if (signCount == 9) {
@@ -65,13 +65,15 @@ const CheckPlayerWin = (event,ind1,ind2) => {
     }
 }
 
-
 const reset = () => {
     pl_One_Data.style.display = "unset";
     pl_Two_Data.style.visibility = "visible";
+    pl_One_Data.style.pointerEvents = "unset";
+    pl_Two_Data.style.pointerEvents = "unset";
     reset_Data.style.visibility = "hidden";
     game_Table.style.visibility = "hidden";
     game_Table.style.pointerEvents = "unset";
+    
     win_Data.innerHTML = "";
     signCount = 0;
     playersign = '';
